3c2d3e17ea0d1d726a168edbfdc7b9a892154a7b
daksh <dakshverma.verma@gmail.com>
2020-06-07 19:17:03 +0530

    added supporting functions and minimax is almost done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+char * * create_board()
+{
+    char * * arr=new char * [3];
+    cout<<"You are 'X'\n";
+    for(int i=0;i<3;i++)
+    {
+        arr[i]=new char[3];
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            arr[i][j]=' ';
+        }
+    }
+    return arr;
+}
 void print_board(char * * arr)
 {
     for(int i=0;i<3;i++)
@@ -34,27 +51,150 @@ bool win_condition(char * * arr)
     }
     return false;
 }
+void player_move(char * * board,char sign="X");
+{
+    while(1)
+    {
+    cout<<"Enter the position: ";
+    short int x;
+    cin>>x;
+    short int row;
+    short int column;
+    find_row_and_column(x,row,column);
+    if(board[row][column]==' ')
+    {
+        board[row][column]=sign;
+        break;
+
+    }
+    else
+    {
+        cout<<"Invalid Move"<<endl;
+    }
+    }
+
+}
+
 int main()
 {
     int n=9;
-    char * * arr=new char *[3];
-    cout<<"You are 'X'\n";
-    for(int i=0;i<3;i++)
-    {
-        arr[i]=new char[3];
-    }
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            arr[i][j]=' ';
-        }
-    }
-   
+    char * * board=create_board();
     while(n--)
     {
-        print_board(arr);
+        print_board(board);
+        cout<<endl;
 
+    }
+}
+// max->0->AI
+void play_move(char ** board,int row,int column,char sign)
+{
+    board[row][column]=sign;
+}
+int best_move(char * * board)
+{
+    int cell=-1;
+    int max_profit=INT_MIN;
+    for(int i=0;i<9;i++)
+    {
+        int row,column;
+        find_row_and_column(i,row,column);
+        if(board[row][column]==' ')
+        {
+            play_move(board,row,column,'O');
+            int profit=minimax(board,false);
+            if(profit>max_profit)
+            {
+                max_profit=profit;
+                cell=i;
+            }
+            //undo move
+            board[row][column]=' ';
+
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return cell;
+
+}
+int minimax(char * * board,bool max)
+{
+    if(win_condition(board))
+    {
+        if(max==true)
+        {
+            return -10;
+        }
+        else
+        {
+            return 10;
+        }
+    }
+    if(!win_condition(board)&&board_full(board))
+    {
+        return 0;
+    }
+    if(max==true)
+    {
+        int max_profit=INT_MIN;
+        for(int i=0;i<9;i++)
+        {
+            
+            int row,column;
+            find_row_and_column(i,row,column);
+            if(board[row][column]==' ')
+            {
+                play_move(board,row,column,'O');
+                int profit=minimax(board,false);
+                if(profit>max_profit)
+                {
+                    max_profit=profit;
+                }
+                //undo move
+                board[row][column]=' ';
+
+            }
+            else
+            {
+                continue;
+            }
+            return max_profit;
+        }
+    }
+    else
+    {
+        int min_profit=INT_MAX;
+        for(int i=0;i<9;i++)
+        {
+            int row,column;
+            find_row_and_column(i,row,column);
+
+        }
+        if(board[row][column]==' ')
+        {
+            play_move(board,row,column,'X');
+            int profit=minimax(board,true);
+            if(profit<min_profit)
+            {
+                min_profit=profit;
+            }
+            
+            
+
+
+            //undo move
+            board[row][column]=' ';
+
+        }
+        else
+        {
+            continue;
+        }
+        return min_profit;
     }
 }
 
