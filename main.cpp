d49ac0b6a149687f92bad05cec2ce3b4c845ed5f
daksh <dakshverma.verma@gmail.com>
2020-06-08 17:45:45 +0530

    completed the development of game

--- main.cpp
+++ main.cpp
@@ -1,5 +1,124 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
+char * * create_board();
+
+void print_board(char * * arr);
+
+bool win_condition(char * * arr);
+
+void player_move(char * * board);
+
+bool board_full(char ** board);
+
+void find_row_and_column(int x,int &row,int &column);
+
+void play_move(char ** board,int row,int column,char sign);
+
+int best_move(char * * board);
+
+int minimax(char * * board,bool max);
+
+void computers_move(char ** board);
+
+int main()
+{
+    cout<<"press 1 if you want to play the first move"<<endl;
+    int turn;
+    cin>>turn;
+    if(turn==1)
+    {
+    int n=9;
+    char * * board=create_board();
+    while(n--)
+    {
+        print_board(board);
+        cout<<endl;
+        if(n%2==0)
+        {
+            player_move(board);
+            if(win_condition(board))
+            {
+                cout<<"Congratulations! You win"<<endl;
+                return 0;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        else
+        {
+            computers_move(board);
+            if(win_condition(board))
+            {
+                print_board(board);
+                cout<<endl;
+                cout<<"You lose! Better luck next time"<<endl;
+                return 0;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    cout<<"Tie"<<endl;
+    return 0;
+    }
+    else
+    {
+        srand(time(NULL));
+        int move=rand()%9+1;
+        char * * board=create_board();
+        int r,c;
+        find_row_and_column(move,r,c);
+        play_move(board,r,c,'O');
+        int n=8;
+        while(n--)
+        {
+            print_board(board);
+            cout<<endl;
+            if(n%2!=0)
+            {
+                player_move(board);
+                if(win_condition(board))
+                {
+                    print_board(board);
+                    cout<<"Congratulations! You win"<<endl;
+                    return 0;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                computers_move(board);
+                if(win_condition(board))
+                {
+                    print_board(board);
+                    cout<<endl;
+                    cout<<"You lose! Better luck next time"<<endl;
+                    return 0;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        cout<<"Tie"<<endl;
+        return 0;
+    }
+    
+}
+
+// max->0->AI
+
+
 char * * create_board()
 {
     char * * arr=new char * [3];
@@ -17,6 +136,7 @@ char * * create_board()
     }
     return arr;
 }
+
 void print_board(char * * arr)
 {
     for(int i=0;i<3;i++)
@@ -24,6 +144,7 @@ void print_board(char * * arr)
         cout<<arr[i][0]<<" "<<"|"<<" "<<arr[i][1]<<" "<<"|"<<" "<<arr[i][2]<<endl;
     }
 }
+
 bool win_condition(char * * arr)
 {
     for(int i=0;i<3;i++)
@@ -51,21 +172,23 @@ bool win_condition(char * * arr)
     }
     return false;
 }
-void player_move(char * * board,char sign="X");
+
+void player_move(char * * board)
 {
+    char sign='X';
     while(1)
     {
     cout<<"Enter the position: ";
-    short int x;
+    int x;
     cin>>x;
-    short int row;
-    short int column;
+    x--;
+    int row;
+    int column;
     find_row_and_column(x,row,column);
     if(board[row][column]==' ')
     {
         board[row][column]=sign;
         break;
-
     }
     else
     {
@@ -75,22 +198,19 @@ void player_move(char * * board,char sign="X");
 
 }
 
-int main()
+void computers_move(char ** board)
 {
-    int n=9;
-    char * * board=create_board();
-    while(n--)
-    {
-        print_board(board);
-        cout<<endl;
-
-    }
+    int cell=best_move(board);
+    int row,column;
+    find_row_and_column(cell,row,column);
+    play_move(board,row,column,'O');
 }
-// max->0->AI
+
 void play_move(char ** board,int row,int column,char sign)
 {
     board[row][column]=sign;
 }
+
 int best_move(char * * board)
 {
     int cell=-1;
@@ -142,8 +262,7 @@ int minimax(char * * board,bool max)
     {
         int max_profit=INT_MIN;
         for(int i=0;i<9;i++)
-        {
-            
+        {   
             int row,column;
             find_row_and_column(i,row,column);
             if(board[row][column]==' ')
@@ -162,8 +281,9 @@ int minimax(char * * board,bool max)
             {
                 continue;
             }
-            return max_profit;
+            
         }
+        return max_profit;
     }
     else
     {
@@ -173,7 +293,7 @@ int minimax(char * * board,bool max)
             int row,column;
             find_row_and_column(i,row,column);
 
-        }
+        
         if(board[row][column]==' ')
         {
             play_move(board,row,column,'X');
@@ -182,10 +302,6 @@ int minimax(char * * board,bool max)
             {
                 min_profit=profit;
             }
-            
-            
-
-
             //undo move
             board[row][column]=' ';
 
@@ -193,6 +309,7 @@ int minimax(char * * board,bool max)
         else
         {
             continue;
+        }
         }
         return min_profit;
     }
